cb3051e22c750b08078a99ce2748dc649767ecd4
MSAnnaM <133233271+MSAnnaM@users.noreply.github.com>
2023-11-11 00:07:16 +0200

    done

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './FriendList.css';
+import { FriendListItem }  from 'components/FriendListItem/FriendListItem';
+
+export const FriendList = ({ friends }) => {
+        return (
+            <ul className="friend-list">
+                {friends.map((friend) => {
+                return <FriendListItem key={friend.id} {...friend}/>
+            })}
+  
+            </ul>);
+    
+}
