05dc98b92f8f06aaa12fba53040ce13e5010d02c
MSAnnaM <133233271+MSAnnaM@users.noreply.github.com>
2023-11-11 20:04:12 +0200

    one more

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -6,7 +6,7 @@ export const FriendList = ({ friends }) => {
   return (
     <ul className="friend-list">
       {friends.map(friend => {
-        return <FriendListItem key={friend.id} {...friend} />;
+        return <FriendListItem key={friend.id} {...friend} />
       })}
     </ul>
   );
